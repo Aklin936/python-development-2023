82eb629bd1f0eaa51a22c460598d7cd7035a4aec
Profile <nil7nenahov72013@gmail.com>
2023-02-23 15:56:26 +0300

    !don't work! added flag for -l, but there is no cow_list

--- cow_say.py
+++ cow_say.py
@@ -3,5 +3,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("message", help="massage that cow says", type=str)
+parser.add_argument("-l", help="list of creatures can be used", action = "store_true" )
 args = parser.parse_args()
-cowsay.cow(args.message)
+if args.l:
+    cowsay.list_cows(args.message)
+else:
+    cowsay.cow(args.message)
