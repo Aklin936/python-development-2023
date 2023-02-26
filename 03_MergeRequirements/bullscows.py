4b3542661554884d3915ca55e13960c55d12b5e6
Profile <nil7nenahov72013@gmail.com>
2023-02-26 18:56:09 +0300

    command python3 -m bullcows ... ... now works

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,5 @@
 import random
+import argparse
 
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls = 0
@@ -15,7 +16,7 @@ def bullscows(guess: str, secret: str) -> (int, int):
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
     random.seed()
-    n = random.randint(0,len(words))
+    n = random.randint(0,len(words)-1)
     secret = words[n]
     trycount = 0
     while True:
@@ -26,3 +27,29 @@ def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
         if bullcowsarr[0] == len(secret):
             break
     ask(str(trycount))
+    return trycount
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    if valid == None:
+        print(prompt)
+        answer = None
+    else:
+        print(prompt)
+        while True:
+            answer = input()
+            f = False 
+            for i in valid:
+                if answer == i:
+                    f = True
+                    break
+            if f:
+                break
+    return(answer)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dict", help="path to the dictionary")
+parser.add_argument("length", help="length of the word")
+args = parser.parse_args()
+print(gameplay(ask, print, ['cat', 'map', 'rap', 'tap']))
